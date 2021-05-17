3a2cfa8e3f3a916131c6f2460b63b45b71d28a50
hakanxemik <hakan.emik@antiloop.com>
2021-05-17 13:05:15 +0200

    adds laravel passport

--- app/Models/User.php
+++ app/Models/User.php
@@ -17,7 +17,10 @@ class User extends Authenticatable
      * @var array
      */
     protected $fillable = [
-        'name',
+        'firstname',
+        'lastname',
+        'gender',
+        'interests',
         'email',
         'password',
     ];
