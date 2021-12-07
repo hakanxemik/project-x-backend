f825f4272971871b2d15500c27861e6629d55315
julian.waldhart@gmail.com <hakan.emik@antiloop.com>
2021-12-07 04:31:50 +0100

    adds default image

--- database/migrations/2014_11_29_000000_create_users_table.php
+++ database/migrations/2014_11_29_000000_create_users_table.php
@@ -23,7 +23,7 @@ class CreateUsersTable extends Migration
             $table->string('email')->unique();
             $table->timestamp('email_verified_at')->nullable();
             $table->string('password');
-            $table->string('bio')->default('Ich bin ein mensch');
+            $table->string('bio')->default('SocialUp ist cool');
             $table->rememberToken();
             $table->timestamps();
         });
